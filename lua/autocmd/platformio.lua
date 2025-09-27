d5494cf2f31fd931f3a6dd7d54b052deb66b03fa
ezulo <ezuloaga0@gmail.com>
2025-09-27 09:46:00 -0400

    Add autocmds conditionally (for windows use)

--- lua/autocmd/platformio.lua
+++ lua/autocmd/platformio.lua
@@ -1,13 +1,16 @@
-piogroup = vim.api.nvim_create_augroup("PlatformIO", {})
-
--- Build
-vim.api.nvim_create_autocmd("BufWritePost", {
-    group = piogroup,
-    pattern = { "**/platformio/**/src/*.c", "**/platformio/**/src/*.cpp" },
-    callback = function()
-        local fp = vim.fn.expand('%:p')
-        local res = vim.fn.system("cpplint "..fp)
-        print(res)
-    end
-})
+if vim.fn.has(win32) then
+    -- Do nothing
+else
+    piogroup = vim.api.nvim_create_augroup("PlatformIO", {})
+    -- cpplint
+    vim.api.nvim_create_autocmd("BufWritePost", {
+        group = piogroup,
+        pattern = { "**/platformio/**/src/*.c", "**/platformio/**/src/*.cpp" },
+        callback = function()
+            local fp = vim.fn.expand('%:p')
+            local res = vim.fn.system("cpplint "..fp)
+            print(res)
+        end
+    })
+end
 
