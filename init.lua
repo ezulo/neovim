17165e6a0b1b9a264aede89baf5a4e4ae66bbd68
ezulo <ezuloaga0@gmail.com>
2025-10-17 15:57:06 -0400

    Add confirm option

--- init.lua
+++ init.lua
@@ -25,6 +25,7 @@ vim.opt.cursorline = true                          -- Highlight current line
 vim.opt.wrap = false                               -- Don't wrap lines
 vim.opt.scrolloff = 10                             -- Keep 10 lines above/below cursor 
 vim.opt.sidescrolloff = 8                          -- Keep 8 columns left/right of cursor
+vim.opt.confirm = true
 
 -- Indentation
 vim.opt.tabstop = 4                                -- Tab width
